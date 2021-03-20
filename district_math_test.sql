f1ea0437b58b89fc470581de19ed6120541fe872
Kate <68763904+Kate-who-codes@users.noreply.github.com>
2021-03-20 11:22:15 -0700

    Pushing data-files that were renamed locally

---
+++ district_math_test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE district_math_test( 
+grade INT,
+studying_year INT,
+demographics VARCHAR,
+number_test INT,
+mean_scale INT)
---
+++ nys_math_test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE nys_math_test(
+grade VARCHAR PRIMARY KEY,
+studying_year  INT,
+demographic VARCHAR,
+number_tested VARCHAR,
+mean_scale INT
+)
+
+SELECT * FROM nys_math_test
